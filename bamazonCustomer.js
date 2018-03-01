370cb81ea5cb47961a6cbbf0cf842482439067a4
Benjamin Cash <cash.benj@gmail.com>
2018-03-01 13:18:34 -0700

    adding demo gifs, updating readME

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const {table} = require("table");
 
-
+//creates connection to mysql database.
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,6 +11,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+//starts a new purchase.
 function newPurchase() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -21,7 +22,10 @@ function newPurchase() {
             var row = [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity];
             data.push(row);
         }
+        console.log("\n");
+        //displays product chart.
         console.log(table(data));
+        //asks what product you would like to purchase.
         inquirer.prompt([
             {
                 type: "input",
@@ -52,15 +56,29 @@ function newPurchase() {
         ]).then(function (res) {
             var productId = res.id;
             var quantityRequested = res.quantity;
+            //selects quantity of chosen product.
             connection.query("SELECT stock_quantity FROM products WHERE ?", { item_id: productId }, function (err, res) {
                 if (err) throw err;
                 var currentQuantity = res[0].stock_quantity;
+                //checks if we have enough product in stock to fill the order.
                 if (quantityRequested > currentQuantity) {
+                    //if there is not enough product in stock, displays error message.
                     console.log("........\n");
                     console.log("Sorry, we don't have enough in stock!");
                     console.log("........\n");
-                    newPurchase();                  
+                    inquirer.prompt([
+                        {
+                            type: "confirm",
+                            message: "Want to make another purchase?",
+                            name: "confirm"
+                        }
+                    ]).then(function(res) {
+                        
+                            console.log("Just take a look at our selection:");
+                            newPurchase();  
+                    })            
                 } else {
+                    //if there is enough product in stock, decreases quantity in database, calculates cost, concludes order.
                     connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: currentQuantity - quantityRequested}, {item_id: productId}], function (err, res) {
                         if (err) throw err;
                         connection.query("SELECT price FROM products WHERE ?", {item_id: productId}, function (err, res) {
@@ -68,8 +86,8 @@ function newPurchase() {
                             var cost = res[0].price * quantityRequested;
                             console.log("........\n");
                             console.log("That will be $" + cost);
-                            console.log("Your order is complete");
-                            console.log("........\n");
+                            console.log("Your order is complete\n");
+                            console.log("........");
                             newPurchase();
                         })
                     })
@@ -79,6 +97,7 @@ function newPurchase() {
     })
 }
 
+//connects to database, and starts the program.
 connection.connect(function (err) {
     if (err) throw err;
     newPurchase();
